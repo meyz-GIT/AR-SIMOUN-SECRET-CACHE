d3b287337c213abdeacd974ff82b8b9572a2e692
meyz-GIT <xbuffex24@gmail.com>
2025-09-15 20:38:12 +0800

    NEW AR FULL

--- Assets/Scripts/ArPositioner.cs
+++ Assets/Scripts/ArPositioner.cs
@@ -3,23 +3,38 @@ using System.Collections.Generic;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class ArPositioner : MonoBehaviour
 {
     public GameObject arscene;
     public XROrigin arorg;
+    public ARPlaneManager planeManager;
+    public bool hasplaced;
     void Start()
     {
 
         // Vector3 playerpos = arorg.camera.transform.position;
-        Vector3 playerpos = arorg.GetComponent<Camera>().transform.position;
-        arscene.transform.position = playerpos;
+       // Vector3 playerpos = arorg.GetComponent<Camera>().transform.position;
+      //  arscene.transform.position = playerpos;
 
     }
 
     
     void Update()
     {
-        
+        if (!hasplaced)
+        {
+            foreach (ARPlane plane in planeManager.trackables)
+            {
+                if (plane.alignment == PlaneAlignment.HorizontalUp)
+                {
+                    arscene.transform.position = plane.center;
+                    arscene.transform.rotation = Quaternion.identity;
+                    hasplaced = true;
+                    break;
+                }
+            }
+        }
     }
 }
