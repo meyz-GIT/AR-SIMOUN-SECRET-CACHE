f2866136328b6f9194a8c0343cd28ecda676ea3b
jiannesantos <santosjianneferryll@gmail.com>
2025-10-11 12:29:44 +0800

    Ar pos

--- Assets/Scripts/ArPositioner.cs
+++ Assets/Scripts/ArPositioner.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using Unity.XR.CoreUtils;
@@ -11,24 +12,20 @@ public class ArPositioner : MonoBehaviour
     public XROrigin arorg;
     public ARPlaneManager planeManager;
     public bool hasplaced;
-
     void Start()
     {
-        if (planeManager != null)
-        {
-            planeManager.enabled = false;
-        }
+
+        // Vector3 playerpos = arorg.camera.transform.position;
+        // Vector3 playerpos = arorg.GetComponent<Camera>().transform.position;
+        //  arscene.transform.position = playerpos;
+
     }
+
 
     void Update()
     {
         if (!hasplaced)
         {
-            if (!planeManager.enabled)
-            {
-                planeManager.enabled = true;
-            }
-
             foreach (ARPlane plane in planeManager.trackables)
             {
                 if (plane.alignment == PlaneAlignment.HorizontalUp)
@@ -38,61 +35,13 @@ public class ArPositioner : MonoBehaviour
                     hasplaced = true;
 
                     planeManager.enabled = false;
-
                     foreach (var p in planeManager.trackables)
                     {
                         p.gameObject.SetActive(false);
                     }
-                    break; 
+                    break;
                 }
             }
         }
     }
 }
-
-//using System.Collections;
- //using System.Collections.Generic;
- //using Unity.XR.CoreUtils;
- //using UnityEngine;
- //using UnityEngine.XR.ARFoundation;
- //using UnityEngine.XR.ARSubsystems;
-
-//public class ArPositioner : MonoBehaviour
-//{
-//    public GameObject arscene;
-//    public XROrigin arorg;
-//    public ARPlaneManager planeManager;
-//    public bool hasplaced;
-//    void Start()
-//    {
-
-//        // Vector3 playerpos = arorg.camera.transform.position;
-//       // Vector3 playerpos = arorg.GetComponent<Camera>().transform.position;
-//      //  arscene.transform.position = playerpos;
-
-//    }
-
-
-//    void Update()
-//    {
-//        if (!hasplaced)
-//        {
-//            foreach (ARPlane plane in planeManager.trackables)
-//            {
-//                if (plane.alignment == PlaneAlignment.HorizontalUp)
-//                {
-//                    arscene.transform.position = plane.center;
-//                    arscene.transform.rotation = Quaternion.identity;
-//                    hasplaced = true;
-
-//                    planeManager.enabled = false;
-//                    foreach(var p in planeManager.trackables)
-//                    {
-//                        p.gameObject.SetActive(false);
-//                    }
-//                    break;
-//                }
-//            }
-//        }
-//    }
-//}
