34ef54638bba1349bf70f412c2ba6c9a1586be4e
jiannesantos <santosjianneferryll@gmail.com>
2025-10-11 11:53:56 +0800

    Level 1 fix arplane

--- Assets/Scripts/ArPositioner.cs
+++ Assets/Scripts/ArPositioner.cs
@@ -11,20 +11,24 @@ public class ArPositioner : MonoBehaviour
     public XROrigin arorg;
     public ARPlaneManager planeManager;
     public bool hasplaced;
+
     void Start()
     {
-
-        // Vector3 playerpos = arorg.camera.transform.position;
-       // Vector3 playerpos = arorg.GetComponent<Camera>().transform.position;
-      //  arscene.transform.position = playerpos;
-
+        if (planeManager != null)
+        {
+            planeManager.enabled = false;
+        }
     }
 
-    
     void Update()
     {
         if (!hasplaced)
         {
+            if (!planeManager.enabled)
+            {
+                planeManager.enabled = true;
+            }
+
             foreach (ARPlane plane in planeManager.trackables)
             {
                 if (plane.alignment == PlaneAlignment.HorizontalUp)
@@ -34,13 +38,61 @@ public class ArPositioner : MonoBehaviour
                     hasplaced = true;
 
                     planeManager.enabled = false;
-                    foreach(var p in planeManager.trackables)
+
+                    foreach (var p in planeManager.trackables)
                     {
                         p.gameObject.SetActive(false);
                     }
-                    break;
+                    break; 
                 }
             }
         }
     }
 }
+
+//using System.Collections;
+ //using System.Collections.Generic;
+ //using Unity.XR.CoreUtils;
+ //using UnityEngine;
+ //using UnityEngine.XR.ARFoundation;
+ //using UnityEngine.XR.ARSubsystems;
+
+//public class ArPositioner : MonoBehaviour
+//{
+//    public GameObject arscene;
+//    public XROrigin arorg;
+//    public ARPlaneManager planeManager;
+//    public bool hasplaced;
+//    void Start()
+//    {
+
+//        // Vector3 playerpos = arorg.camera.transform.position;
+//       // Vector3 playerpos = arorg.GetComponent<Camera>().transform.position;
+//      //  arscene.transform.position = playerpos;
+
+//    }
+
+
+//    void Update()
+//    {
+//        if (!hasplaced)
+//        {
+//            foreach (ARPlane plane in planeManager.trackables)
+//            {
+//                if (plane.alignment == PlaneAlignment.HorizontalUp)
+//                {
+//                    arscene.transform.position = plane.center;
+//                    arscene.transform.rotation = Quaternion.identity;
+//                    hasplaced = true;
+
+//                    planeManager.enabled = false;
+//                    foreach(var p in planeManager.trackables)
+//                    {
+//                        p.gameObject.SetActive(false);
+//                    }
+//                    break;
+//                }
+//            }
+//        }
+//    }
+//}
--- Assets/Scripts/Memory/PauseManager.cs
+++ Assets/Scripts/Memory/PauseManager.cs
@@ -5,7 +5,7 @@ public class PauseManager : MonoBehaviour
 {
     [SerializeField] private ARSession arSession;
 
-    [SerializeField] private ArPositioner arPositioner; 
+    //[SerializeField] private ArPositioner arPositioner; 
 
     private bool isPaused = false;
 
@@ -15,10 +15,10 @@ public class PauseManager : MonoBehaviour
         {
             Debug.LogError("AR Session not assigned to PauseManager!");
         }
-        if (arPositioner == null)
-        {
-            Debug.LogError("Ar Positioner not assigned to PauseManager!");
-        }
+        //if (arPositioner == null)
+        //{
+        //    Debug.LogError("Ar Positioner not assigned to PauseManager!");
+        //}
     }
     
     public void TogglePause()
@@ -55,15 +55,15 @@ public class PauseManager : MonoBehaviour
         }
 
         
-        if (arPositioner != null && arPositioner.hasplaced)
-        {
-            arPositioner.planeManager.enabled = false;
-            foreach (var p in arPositioner.planeManager.trackables)
-            {
-                p.gameObject.SetActive(false);
-            }
-            Debug.Log("Planes re-hidden after resuming AR Session.");
-        }
+        //if (arPositioner != null && arPositioner.hasplaced)
+        //{
+        //    arPositioner.planeManager.enabled = false;
+        //    foreach (var p in arPositioner.planeManager.trackables)
+        //    {
+        //        p.gameObject.SetActive(false);
+        //    }
+        //    Debug.Log("Planes re-hidden after resuming AR Session.");
+        //}
 
         Time.timeScale = 1f;
         Debug.Log("Game Resumed.");
